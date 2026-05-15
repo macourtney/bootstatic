0fd4526c8a2b91a3bcea8129d246a32b37226607
Matthew Courtney <matt-git@port87.com>
2026-05-15 17:06:19 -0400

    Updated main.py to copy static files in to the public directory. Added the public directory to .gitignore.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from blocks import markdown_to_blocks, block_to_block_type
 from parentnode import ParentNode, block_type_to_html_node
 from textnode import TextNode, TextNodeType
-
+import shutil
+import os
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
@@ -11,10 +12,26 @@ def markdown_to_html_node(markdown):
         html_nodes.append(block_type_to_html_node(block_type, block))
     return ParentNode("div", html_nodes)
 
+def delete_public():
+    print("Deleting public")
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+
+def copy_static_files():
+    print("Copying static files")
+    if os.path.exists("static"):
+        shutil.copytree("static", "public")
+
 
 def main():
-    tnode1 = TextNode("This is some anchor text", TextNodeType.LINK, "https://www.boot.dev")
-    return tnode1
+    delete_public()
+    copy_static_files()
+    return """
+    Scan content/
+    Find all markdown files
+    Convert each markdown file to an html file
+    Create a public/ folder for all the html files
+    """
 
 if __name__ == "__main__":
     print(main())
