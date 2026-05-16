dae2beee2e1b3dbcd938e33165638ccf51745427
Matthew Courtney <matt-git@port87.com>
2026-05-16 13:19:54 -0400

    Added build.sh. Changed main.py to move files to the docs directory instead of the public directory. Added the docs directory so github will pick it up.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-import genericpath
+import sys
+import shutil
+import os
 from extract import extract_title
 from blocks import markdown_to_blocks, block_to_block_type
 from parentnode import ParentNode, block_type_to_html_node
-from textnode import TextNode, TextNodeType
-import shutil
-import os
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
@@ -14,17 +13,17 @@ def markdown_to_html_node(markdown):
         html_nodes.append(block_type_to_html_node(block_type, block))
     return ParentNode("div", html_nodes)
 
-def delete_public():
-    print("Deleting public")
-    if os.path.exists("public"):
-        shutil.rmtree("public")
+def delete_docs():
+    print("Deleting docs")
+    if os.path.exists("docs"):
+        shutil.rmtree("docs")
 
 def copy_static_files():
     print("Copying static files")
     if os.path.exists("static"):
-        shutil.copytree("static", "public")
+        shutil.copytree("static", "docs")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(base_path, from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path}")
     with open(from_path, "r") as f:
         markdown = f.read()
@@ -32,33 +31,39 @@ def generate_page(from_path, template_path, dest_path):
         template = f.read()
 
     html_node = markdown_to_html_node(markdown)
-    print("HTML NODE: ", html_node)
     html_content = html_node.to_html()
 
     title = extract_title(markdown)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html_content)
+    template = template.replace("href=\"", f"href=\"{base_path}")
+    template = template.replace("src=\"", f"src=\"{base_path}")
 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as f:
         f.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(base_path, dir_path_content, template_path, dest_dir_path):
     markdown_extensions = [".md"]
     
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         if os.path.isdir(from_path):
-            generate_pages_recursive(from_path, template_path, os.path.join(dest_dir_path, filename))
+            generate_pages_recursive(base_path, from_path, template_path, os.path.join(dest_dir_path, filename))
         elif filename.endswith(tuple(markdown_extensions)):
             dest_path = os.path.join(dest_dir_path, filename[:-3] + ".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(base_path, from_path, template_path, dest_path)
 
 def main():
-    delete_public()
+    if len(sys.argv) != 2:
+        raise ValueError("Usage: python3 main.py <base_path>")
+    base_path = sys.argv[1]
+    if len(base_path) == 0:
+        base_path = "/"
+    delete_docs()
     copy_static_files()
 
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive(base_path, "content", "template.html", "docs")
     return "Generating static files complete!"
 
 if __name__ == "__main__":
